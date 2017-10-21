d0e77955b4b3bc6843a40d677407df9311a8a78d
Anolan19 <anolan19@terpmail.umd.edu>
2017-10-21 05:45:49 -0400

    new min android version 22 for required library functions,  sms functions work in progress (ie working on a tutorial)

---
+++ app/src/main/java/com/example/adam/old_person_helper/Texting.java
@@ -0,0 +1,92 @@
+package com.example.adam.old_person_helper;
+import android.Manifest;
+import  	android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.*;
+
+import java.util.ArrayList;
+
+/**
+ * Created by adam on 10/21/17.
+ */
+
+public class Texting {
+    private SharedPreferences preferences;
+
+
+}
+public class Text_Screen extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void getPermissionToReadSMS() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (shouldShowRequestPermissionRationale(
+                    Manifest.permission.READ_SMS)) {
+                Toast.makeText(this, "Please allow permission!", Toast.LENGTH_SHORT).show();
+            }
+            requestPermissions(new String[]{Manifest.permission.READ_SMS},
+                    READ_SMS_PERMISSIONS_REQUEST);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        // Make sure it's our original READ_CONTACTS request
+        if (requestCode == READ_SMS_PERMISSIONS_REQUEST) {
+            if (grantResults.length == 1 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Read SMS permission granted", Toast.LENGTH_SHORT).show();
+                refreshSmsInbox();
+            } else {
+                Toast.makeText(this, "Read SMS permission denied", Toast.LENGTH_SHORT).show();
+            }
+
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+
+
+
+
+
+
+
+    ArrayList<String> smsMessagesList = new ArrayList<>();
+    ListView messages;
+    ArrayAdapter arrayAdapter;
+    private static final int READ_SMS_PERMISSIONS_REQUEST = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.text_screen); //text_screen is the name of the screen for texting
+        messages = (ListView) findViewById(R.id.messages); //messages is the list of the messages in the texting screen
+        input = (EditText) findViewById(R.id.input);
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, smsMessagesList);
+        messages.setAdapter(arrayAdapter);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            getPermissionToReadSMS();
+        } else {
+            refreshSmsInbox();
+        }
+    }
+
