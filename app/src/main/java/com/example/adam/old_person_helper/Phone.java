dc27d2510837112b53b4004ea5bc1cdee3e91b95
Anolan19 <anolan19@terpmail.umd.edu>
2017-10-21 06:44:46 -0400

    commented out text class ,added phone class

---
+++ app/src/main/java/com/example/adam/old_person_helper/Phone.java
@@ -0,0 +1,20 @@
+package com.example.adam.old_person_helper;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+/**
+ * Created by adam on 10/21/17.
+ */
+
+public class Phone {
+
+
+}
+
